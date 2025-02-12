7e7d8bc47b52882171123795b1cf52bac879bc37
ColinKnox <colinwknox@gmail.com>
2025-02-11 20:50:39 -0500

    Made a program that's just a conversation based on what's inputted into a function.

---
+++ math_question.py
@@ -0,0 +1,5 @@
+def math_conversation(value1, value2, student, teacher):
+    multiply = value1 * value2
+    print(f"{student}: Ms. {teacher}. What's {value1} times {value2}? \nMs. {teacher}: Why it's {multiply}, of course!")
+
+math_conversation(2, 5, "Dan", "Kris")
